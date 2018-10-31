8bd9bcd3e38f61bc3b8df1676e2eedc429a86da8
Margaret Thurston <mthursto@u.rochester.edu>
2018-10-31 13:38:41 -0400

    Added z-page, buttons, and images
    (still need to do background image)

--- index.php
+++ index.php
@@ -6,7 +6,31 @@
 	?>
 <body>
 
-	<p>testing</p>
+	<header>
+		<h1>Halloween</h1>
+
+		<button>
+			<a href="about.php">Read More</a>
+		</button>
+
+		<button>
+			<a>Sign up for our Holiday Newsletter</a>
+		</button>
+	</header>
+
+	<main>
+		<figure>
+			<img src="images/halloween-illustration.jpg">
+		</figure>
+	</main>
+
+	<footer>
+		<h2>Carvings! Costumes! Candy!</h2>
+		<p>Take a tour through Margaret's Halloween past...</p>
+		<button>
+			<a href="about.php">Read More</a>
+		</button>
+	</footer>
 
 
 
