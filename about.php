871b702aa97eb5941ccf4af973ba3abc3e09c8e0
Margaret Thurston <mthursto@u.rochester.edu>
2018-10-30 23:45:15 -0400

    Added content to about page

--- about.php
+++ about.php
@@ -6,7 +6,51 @@
 	?>
 <body>
 
-	<p>testing</p>
+	<h1>The Best Parts of Halloween... From my ghostly past...</h1>
+
+	<section>
+		<h2>Carvings</h2>
+			<h3>What Happens</h3>
+				<p>Before Halloween night, many people carve pumpkins. They cut off the top, take out the pulp and seeds, and carve out a face or other image on one side. On Halloween night, they stick a candle or a light inside, and it sits on the doorstep to greet trick-or-treaters.</p>
+			<h3>A Halloween Story</h3>
+				<p>When I was a kid, we would always wait until Halloween to carve the pumpkin. We would put newspaper on the floor, do the seed-scooping and face-drawing, and my dad would do the carving. We never got much more creative than faces, but it sure was fun!</p>
+			<figure>
+				<img src="images/jack-o-lanterns.jpg" alt="Jack-O-Lanterns">
+			</figure>
+				
+	</section>
+
+
+
+	<section>
+		<h2>Costumes</h2>
+			<h3>What Happens</h3>
+				<p>When it starts to get dark, kids put on their costumes. Ghosts, witches, monsters, fairies, and more go roaming the night, bags slung on their arms. Parents supervise. (They don’t need costumes; they’re scary enough.)</p>
+			<h3>A Halloween Story</h3>
+				<p>One Halloween my brother was Mr. Tickle, a story-book character with long arms who could tickle people from afar. He attached strings of cardboard paper towel tubes to his arms. His new “arms” kept dragging on the ground, though, which was kind of annoying.</p>
+			<figure>
+				<img src="images/halloween_costumes.jpg" alt="Halloween Costumes">
+			</figure>
+				
+	</section>
+
+
+
+	<section>
+		<h2>Candy</h2>
+			<h3>What Happens</h3>
+				<p>The doorbell rings. “Trick-or-treat!” kids say when the door opens. Richer by a piece of candy or two, the kids say thank you and move on to the next house. That night it is taste-buds galore… and hopefully no stomachaches!</p>
+			<h3>A Halloween Story</h3>
+				<p>My dentist used to tell me that I could eat your Halloween candy for 5 days, and then afterwards I should put it out on the doorstep because the Great Pumpkin would come around and collect it. One year I actually did this. Awhile later I asked my mom for the candy, but it was already in the Great Pumpkin’s stomach.</p>
+			<figure>
+				<img src="images/candy.jpg" alt="Halloween Candy">
+			</figure>
+				
+	</section>
+
+
+
+	
 
 
 
